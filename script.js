8fbd2e100da47a57807d643a1062c55d6d884a38
Nicolas Lema <n.lema@uniandes.edu.co>
2020-03-03 22:03:47 -0500

    Funcionalidad de limpiar paleta

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
-function randomPalette(){
+const textoInicial = `
+.website-background{ color: #FFFFFF;}
+
+.element-text{ color: #FFFFFF;}
+
+.element-border{ border-color: #FFFFFF;}
+
+.element-background{ background-color: #FFFFFF;}
+
+.header{ color: #FFFFFF;}
+
+`;
+
+
+function randomPalette() {
 
 }
 
 
 
-function generateRules(){
+function generateRules() {
+
+}
+
+function limpiarPaleta() {
+
+    var areaTexto = document.getElementById("css-rules");
+    areaTexto.value = textoInicial;
+    for (let i = 1; i <= 5; i++) {
+        var idDinamico = "color" + i;
+        quitarClase(idDinamico);
+    }
+
+}
+
+function quitarClase(elementId) {
+
+    document.getElementById(elementId).className = "color-view";
+}
+
+function generarPaleta(){
+    
 
 }
