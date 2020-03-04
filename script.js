2d2f4b88bc118f5bd96b4a4ac1878f2986178dcb
Nicolas Lema <n.lema@uniandes.edu.co>
2020-03-03 23:41:28 -0500

    Generar colores aleatorios sin tener en cuenta armonia

--- script.js
+++ script.js
@@ -11,34 +11,87 @@ const textoInicial = `
 
 `;
 
-
-function randomPalette() {
-
-}
-
-
-
-function generateRules() {
-
-}
-
 function limpiarPaleta() {
 
     var areaTexto = document.getElementById("css-rules");
     areaTexto.value = textoInicial;
     for (let i = 1; i <= 5; i++) {
         var idDinamico = "color" + i;
-        quitarClase(idDinamico);
+        asignarClase(idDinamico, "color-view");
+        quitarEstilo(idDinamico);
+    }
+    document.body.removeAttribute("style");
+
+    var h2 = document.querySelectorAll("h2");
+    for (i = 0; i < h2.length; i++) {
+        h2[i].removeAttribute("style");
     }
 
 }
 
-function quitarClase(elementId) {
+function asignarClase(elementId, clase) {
 
-    document.getElementById(elementId).className = "color-view";
+    document.getElementById(elementId).className = clase;
 }
 
-function generarPaleta(){
-    
+function quitarEstilo(elementId) {
+    document.getElementById(elementId).removeAttribute("style");
+}
 
+function generarPaleta() {
+
+    var cajitas = document.querySelectorAll(".color-view");
+
+    //Cambiar el fondo de la pagina
+    var colorFondo = RGBRandom();
+    document.body.style.backgroundColor = colorFondo;
+
+    //Cambiar el color del texto de los elementos
+    var colorTexto = RGBRandom();
+    for (i = 0; i < cajitas.length; i++) {
+        cajitas[i].style.color = colorTexto;
+    }
+
+    //Cambiar el color del borde de los elementos
+    var colorBorde = RGBRandom();
+    for (i = 0; i < cajitas.length; i++) {
+        cajitas[i].style.borderColor = colorBorde;
+    }
+
+    //Cambiar el color del fondo de los elementos
+    var colorFondoElementos = RGBRandom();
+    for (i = 0; i < cajitas.length; i++) {
+        cajitas[i].style.backgroundColor = colorFondoElementos;
+    }
+
+
+    //Cambiar el texto del encabezado
+    var colorH2 = RGBRandom();
+    var h2 = document.querySelectorAll("h2");
+    for (i = 0; i < h2.length; i++) {
+        h2[i].style.color = colorH2;
+    }
+
+    var areaTexto = document.getElementById("css-rules");
+    var textoVariable = `
+.website-background{ color: `+colorFondo+`;}
+
+.element-text{ color: `+colorTexto+`;}
+
+.element-border{ border-color: `+colorBorde+`;}
+
+.element-background{ background-color: `+colorFondoElementos+`;}
+
+.header{ color: `+colorH2+`;}
+
+`;
+    areaTexto.value = textoVariable;
+
+}
+
+function RGBRandom() {
+    var r = Math.floor(Math.random() * 256);          // Random between 0-255
+    var g = Math.floor(Math.random() * 256);          // Random between 0-255
+    var b = Math.floor(Math.random() * 256);          // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
