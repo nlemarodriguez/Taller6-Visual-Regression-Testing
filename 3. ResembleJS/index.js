19757019f74c734c6b35c7ca8d4ffa3e5434055b
Nicolas Lema <n.lema@uniandes.edu.co>
2020-03-14 15:31:14 -0500

    Comparacion de imagenes

--- 3. ResembleJS/index.js
+++ 3. ResembleJS/index.js
@@ -1,11 +1,15 @@
-var api = resemble(fileData).onComplete(function(data) {
-    console.log(data);
-    /*
-	{
-	  red: 255,
-	  green: 255,
-	  blue: 255,
-	  brightness: 255
-	}
-	*/
-});
+const resemble = require("resemblejs");
+const fs = require('fs');
+
+imagen1_login_correcto = "./imagenes/1. antes/Login_correcto_antes.png"
+imagen2_login_correcto = "./imagenes/2. despues/Login_correcto_despues.png"
+imagenResultado_login_correcto = "./imagenes/3. comparacion/Login_correcto.png"
+
+var diff = resemble(imagen1_login_correcto)
+    .compareTo(imagen2_login_correcto)
+    .ignoreLess()
+    .onComplete(function(data) {
+        fs.writeFileSync(imagenResultado_login_correcto, data.getBuffer());
+        console.log(data);
+    });
+
